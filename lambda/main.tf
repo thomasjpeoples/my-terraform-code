cfebadb8610d2cc8dc93e1faf07c836b2f7ddb8f
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-10 13:45:25 +0000

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,8 +1,14 @@
+data "archive_file" "lambda_code" {
+  type        = "zip"
+  source_dir  = "./lambda"  # The directory containing your Lambda function code
+  output_path = "${path.module}/lambda.zip"
+}
+
 resource "aws_lambda_function" "lambda_function" {
   function_name    = var.lambda_function_name
   handler          = var.lambda_handler
   runtime          = var.lambda_runtime
   role             = var.lambda_role_arn
-  filename         = var.lambda_zip_file
-  source_code_hash = filebase64sha256(var.lambda_zip_file)
+  filename         = data.archive_file.lambda_code.output_path
+  source_code_hash = filebase64sha256(data.archive_file.lambda_code.output_path)
 }
