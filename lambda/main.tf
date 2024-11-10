bef34432b2dbc8f3567f0c714af147f08ce1570c
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-10 14:11:05 +0000

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -8,6 +8,7 @@ resource "aws_lambda_function" "lambda_function" {
   function_name    = var.lambda_function_name
   handler          = var.lambda_handler
   runtime          = var.lambda_runtime
+  role             = var.lambda_role_arn
   filename         = data.archive_file.lambda_code.output_path
   source_code_hash = filebase64sha256(data.archive_file.lambda_code.output_path)
 }
