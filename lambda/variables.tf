600a7649247b8dfba1d27cae02325247de3d65d7
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-10 13:42:35 +0000

    Create variables.tf

---
+++ lambda/variables.tf
@@ -0,0 +1,24 @@
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "lambda_handler" {
+  description = "The handler for the Lambda function"
+  type        = string
+}
+
+variable "lambda_runtime" {
+  description = "The runtime for the Lambda function"
+  type        = string
+}
+
+variable "lambda_role_arn" {
+  description = "The ARN of the IAM role assumed by Lambda"
+  type        = string
+}
+
+variable "lambda_zip_file" {
+  description = "Path to the zipped Lambda function code"
+  type        = string
+}
