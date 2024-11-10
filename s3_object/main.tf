80a3a15b0481ab97b94585cb098cf9eac55fe545
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-10 18:45:59 +0000

    Update main.tf

--- s3_object/main.tf
+++ s3_object/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_object" "file_upload" {
-  bucket = var.bucket_name
-  key    = var.object_key
-  source = var.source_path
-  acl    = var.acl
+  bucket_name = var.bucket_name
+  object_key  = var.object_key
+  source_path = var.source_path
+  acl         = var.acl
 }
