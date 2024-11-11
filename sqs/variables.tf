2b94c7ae486c144fdc196d738fa437a5098c5801
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-11 10:19:42 +0000

    Update variables.tf

--- sqs/variables.tf
+++ sqs/variables.tf
@@ -1,3 +1,8 @@
+variable "bucket_arn" {
+  description = "ARN of the S3 bucket for the trigger"
+  type        = string
+}
+
 variable "sqs_queue_name" {
   description = "Name of the SQS main queue"
   type        = string
