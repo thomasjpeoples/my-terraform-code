1e2c3e915b088f4ed605e51d70a035b97bae0e97
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-11 06:14:35 +0000

    Update and rename layer.tf to variables.tf

---
+++ lambda_layer/variables.tf
@@ -0,0 +1,11 @@
+variable "layer_name" {
+  description = "Name of the Lambda layer"
+}
+
+variable "s3_bucket" {
+  description = "S3 bucket name for the Lambda layer zip"
+}
+
+variable "s3_key" {
+  description = "S3 key for the Lambda layer zip"
+}
