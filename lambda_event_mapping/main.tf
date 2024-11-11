dc1e828b975812186c59603c75cc22f8db87c72d
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-11 12:37:53 +0000

    Update main.tf

--- lambda_event_mapping/main.tf
+++ lambda_event_mapping/main.tf
@@ -1,6 +1,6 @@
 # SQS Trigger for Lambda
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
-  event_source_arn = var.sqs_queue.arn
+  event_source_arn = var.sqs_queue_arn
   function_name    = var.lambda_function_name
   batch_size       = var.batch_size
   enabled          = var.enabled
