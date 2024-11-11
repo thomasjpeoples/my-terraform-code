f81541866d52156ce8b533fb8f1fc5b1905a0847
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-11 10:31:15 +0000

    Update outputs.tf

--- sqs/outputs.tf
+++ sqs/outputs.tf
@@ -5,3 +5,11 @@ output "sqs_queue_url" {
 output "sqs_dlq_url" {
   value = aws_sqs_queue.dlq.url
 }
+
+output "queue_arn" {
+  value = aws_sqs_queue.main_queue.arn
+}
+
+output "dlq_arn" {
+  value = aws_sqs_queue.dlq.arn
+}
