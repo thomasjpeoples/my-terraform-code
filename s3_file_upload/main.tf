c9f34f04528c47913045d2eddbff26730b53d7b7
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-17 17:30:10 +0000

    Update main.tf

--- s3_file_upload/main.tf
+++ s3_file_upload/main.tf
@@ -1,6 +1,6 @@
 # Upload any file (e.g., .txt, .csv, .json, etc.) to a specific S3 path
 
-resource "aws_s3_object" "file_upload" {
+resource "aws_s3_objects" "file_upload" {
   bucket = var.bucket_name
   key    = "${var.s3_prefix}/${basename(var.file_path)}"  # Retains the original file name
   source = var.file_path
