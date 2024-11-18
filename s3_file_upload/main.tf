2400fa4cda74fe43ccad67a2d6ca6ee109f81bfb
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-18 06:18:19 +0000

    Update main.tf

--- s3_file_upload/main.tf
+++ s3_file_upload/main.tf
@@ -1,7 +1,8 @@
 # Module to upload a single file to an S3 bucket
 resource "aws_s3_bucket_object" "file_upload" {
-  bucket = var.bucket_name
-  key    = "${var.s3_prefix}/${basename(var.file_path)}"  # Retains the original file name
-  source = var.file_path
-  etag   = filebase64sha256(var.file_path)  # Hash the content of the file before upload
+  bucket       = var.bucket_name
+  key          = "${var.s3_prefix}/${basename(var.file_path)}"
+  source       = var.file_path
+  etag         = filebase64sha256(var.file_path)
+  content_type = var.content_type
 }
