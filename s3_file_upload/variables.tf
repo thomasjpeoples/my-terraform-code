d2b19ffb385e014d13d4d85e04dd323054a612b7
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2024-11-18 06:16:59 +0000

    Update variables.tf

--- s3_file_upload/variables.tf
+++ s3_file_upload/variables.tf
@@ -12,3 +12,9 @@ variable "file_path" {
   description = "The local path to the file to upload (could be .txt, .csv, .json, etc.)."
   type        = string
 }
+
+variable "content_type" {
+  description = "The MIME type of the file to upload (e.g., application/json)"
+  type        = string
+  default     = "application/octet-stream"  # Default type if not provided
+}
